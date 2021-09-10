249db619ebb500c21fbc917cc7a9c3681efbd15f
CameronBernier <cbernier@overlake.org>
2021-09-10 14:02:27 -0700

    Tests printing function

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("A Review of Behind the Frame: The Finest Scenery")
+print("Behind the frame was probably one of the most visually stunning games I have played.")
+print("From the hand-animated characters to the detailed backgrounds, the whole game was gorgeous.")
+print("Not only that but the soundtrack was to die for. \n\"The Color You Love The Most\" was my favorite.")
