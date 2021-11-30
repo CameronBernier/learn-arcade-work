1846368b690637e7a135ee8a4d03260b750e88aa
Cameron Bernier <89711505+CameronBernier@users.noreply.github.com>
2021-11-30 08:44:43 -0800

    Advanced Looping Problems

---
+++ Practice/Advanced Looping Problems.py
@@ -0,0 +1,78 @@
+# Problem 1
+print("#1")
+for row in range(10):
+    print("*", end=" ")
+
+print()
+print()
+
+# Problem 2
+print("#2")
+for row in range(10):
+    print("*", end=" ")
+print()
+for row in range(5):
+    print("*", end=" ")
+print()
+for row in range(20):
+    print("*", end=" ")
+print()
+
+print()
+
+# Problem 3
+print("#3")
+for row in range(10):
+    print()
+    for column in range(10):
+        print("*", end=" ")
+
+print()
+print()
+
+# Problem 4
+print("#4")
+
+for row in range(10):
+    print()
+    for column in range(5):
+        print("*", end=" ")
+
+print()
+print()
+
+# Problem 5
+print("#5")
+
+for row in range(5):
+    print()
+    for column in range(20):
+        print("*", end=" ")
+
+print()
+print()
+
+# Problem 6
+print("#6")
+for row in range(10):
+    print()
+    for column in range(10):
+        print(column, end=" ")
+
+print()
+print()
+
+# Problem 7
+print("#7")
+x = 0
+for row in range(10):
+    print()
+    for column in range(10):
+        print(x, end=" ")
+    x = x + 1
+
+print()
+
+
+
+
