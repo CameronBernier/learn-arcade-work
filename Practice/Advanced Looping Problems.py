9467823abf867b4737d466e15956dde250887fbc
Cameron Bernier <89711505+CameronBernier@users.noreply.github.com>
2021-12-02 08:40:21 -0800

    Advanced Looping Problems

--- Practice/Advanced Looping Problems.py
+++ Practice/Advanced Looping Problems.py
@@ -1,7 +1,9 @@
 # Problem 1
 print("#1")
-for row in range(10):
+i = 1
+while i <= 10:
     print("*", end=" ")
+    i += 1
 
 print()
 print()
@@ -43,36 +45,45 @@ print()
 
 # Problem 5
 print("#5")
-
-for row in range(5):
-    print()
-    for column in range(20):
-        print("*", end=" ")
-
+o = 0
+p = 0
+while o < 5:
+    print(' ')
+    o += 1
+    p = 0
+    while p < 20:
+        print(end=' *')
+        p += 1
 print()
 print()
 
 # Problem 6
 print("#6")
-for row in range(10):
-    print()
-    for column in range(10):
-        print(column, end=" ")
+q = 0
+w = 0
+while q < 10:
+    print(' ')
+    q += 1
+    w = 0
+    while w < 10:
+        number = str(w)
+        print(' ', end=number)
+        w += 1
 
 print()
 print()
 
 # Problem 7
 print("#7")
-x = 0
-for row in range(10):
-    print()
-    for column in range(10):
-        print(x, end=" ")
-    x = x + 1
+row = -1
+column = 0
+while row < 9:
+    print(' ')
+    row += 1
+    column = 0
+    while column < 10:
+        number1 = str(row)
+        print(' ', end=number1)
+        column += 1
 
 print()
-
-
-
-
--- Testing/test.py
+++ Testing/test.py
@@ -1,7 +1,2 @@
-total = 0
 
-for i in range(10):
-	new_number = int(input("Enter a number."))
-	total = total + new_number
 
-print("The total is: ", total)
